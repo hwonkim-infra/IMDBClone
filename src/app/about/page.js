22caf0eea3bb8f6ebc418c85ede2fd28ae5d7e60
hwonkim-infra <52963259+hwonkim-infra@users.noreply.github.com>
2023-05-27 16:59:33 +0900

    fetch data from tmdb api in the homePage

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1><p>
+
+      Not all drug consumption helps people work better, of course. Don Draper from “Mad Men”, a tv series about advertising executives in the 1960s, came across many of his finest ideas three Scotches deep. But contrary to popular belief Ernest Hemingway, one of America’s greatest authors, never advised “write drunk, edit sober”, preferring to write liquor-free. In a book published in 1983 David Ogilvy, perhaps the most famous real-life mad man, warned of the dangers of drunks in the office. Use of cocaine, common on Wall Street and in Hollywood, can give people a short-term boost. It also causes grave long-term problems.
+      </p>
+      <p>Indeed, economists normally think of mood-altering substances as a drag on prosperity. One estimate in 2007 put the cost of drug abuse in America at $193bn, or around 1.3% of gdp. More recently economists have looked at “deaths of despair”, which many link to abuse of opioids. In 2021 more than 80,000 Americans died from opioid overdoses.</p>
+    </div>
+  );
+}
